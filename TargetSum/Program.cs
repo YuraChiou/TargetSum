624c41f3906df0169cfde416145c776cad7398ac
Yura Chiou <qdhfire@hotmail.com>
2022-06-20 15:22:39 +0800

    Add project files.

---
+++ TargetSum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TargetSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string description = "\nType an array of integers nums and an integer target then press Enter."
+                                + "Type ';' to end the array:\n";
+
+            try
+            {
+                List<int> data = new List<int>();
+                int target = 0;
+                string input = null;
+
+                do
+                {
+                    Console.WriteLine(description);
+
+                    input = Console.ReadLine();
+
+                    string[] inputArray = input.Split(";");
+
+                    if (inputArray.Length == 2)
+                    {
+                        data = inputArray[0].Split(',')
+                            ?.Select(x =>
+                            {
+                                int.TryParse(x, out int interge);
+                                return interge;
+                            })?.ToList();
+
+                        int.TryParse(inputArray[1], out target);
+
+                        Service _service = new Service();
+
+                        _service.TargetSum(data, target);
+                    }
+                } while (string.IsNullOrWhiteSpace(input) || !input.Contains(';'));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception {0}.", ex.Message);
+                Console.WriteLine(description);
+            }
+        }
+    }
+}
---
+++ TargetSum/Service.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TargetSum
+{
+    public class Service
+    {
+        /// <summary>
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// </summary>
+        /// <param name="data">An array of integers nums</param>
+        /// <param name="target">An integer target</param>
+        /// <returns>Return indices of the two numbers such that they add up to target.</returns>
+        public void TargetSum(List<int> data, int target)
+        {
+            try
+            {
+
+                // Input: List<int> data = new List<int> { 1, 2, 3, 4, 5, 6 };
+                // Target: int target = 10;
+                // Output: [3, 5]
+                Console.WriteLine($"Input: { string.Join(", ", data) }");
+                Console.WriteLine($"Target: { target }");
+
+                Dictionary<int, int> output = new Dictionary<int, int>();
+                Dictionary<int, int> dataDictionary = data.ToDictionary(x => x);
+
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    int augend = target - data[i];
+
+                    if (dataDictionary.ContainsValue(augend))
+                    {
+                        int indexOfAugend = Array.IndexOf(dataDictionary.Values.ToArray(), augend);
+                        output.Add(i, indexOfAugend);
+
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"Output: [{ output.First().Key }, { output.First().Value }]");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception {0}.", ex.Message);
+            }
+        }
+    }
+}
